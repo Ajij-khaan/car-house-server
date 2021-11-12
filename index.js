db17cc1dc8b18e4fb02aaa7569efe0b32c0115f4
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-11-12 21:39:06 +0600

    data inserted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,32 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ludk7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// console.log(uri)
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("Car_House");
+        const carsCollection = database.collection("Cars");
+
+        //Get Cars Collection APi
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find({});
+            const cars = await cursor.toArray();
+            res.send(cars);
+        })
+
+    }
+
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 
 
