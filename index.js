5abc3034030f280a70e5647747a3dd5e5c8817e9
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-11-13 12:30:55 +0600

    order

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,6 +38,15 @@ async function run() {
             const ManageOrder = await cursor.toArray();
             res.send(ManageOrder);
         })
+
+        //Delete Order
+        app.delete('/manageorder/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query);
+            res.json(result);
+        })
+
 
         app.post('/manageorder', async (req, res) => {
             const newOrder = req.body;
