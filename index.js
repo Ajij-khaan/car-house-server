285b02ae04075da9148cd1ff723558a8d504b3fa
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-11-15 12:07:30 +0600

    admin

--- index.js
+++ index.js
@@ -111,6 +111,17 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
+
+        //checking admin
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') { isAdmin = true; }
+            res.json({ admin: isAdmin });
+        })
     }
 
     finally {
